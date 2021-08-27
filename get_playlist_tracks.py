5885d052d18d684c073c66ba2b8495e737d1312f
soonerfan237 <ou1/s00n>
2021-08-27 17:53:59 -0500

    Configuration fix

--- get_playlist_tracks.py
+++ get_playlist_tracks.py
@@ -1,10 +1,8 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-def get_playlist_tracks(user_id, playlist_id):
+def get_playlist_tracks(user_id, playlist_id, cid, secret):
     sp = spotipy.Spotify()
-    cid = '5553faba0c3a447abc4eb54bda441322'
-    secret = 'e961dd2b417344ed8b51e4eccc4c025d'
     client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
     sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
     sp.trace=False
--- main.py
+++ main.py
@@ -4,8 +4,10 @@ import read_config
 
 user_id = read_config.get_user_id()
 playlist_id = read_config.get_playlist_id()
+cid = read_config.get_cid()
+secret = read_config.get_secret()
 
-spotify_playlist_songs = get_playlist_tracks.get_playlist_tracks(user_id,playlist_id)
+spotify_playlist_songs = get_playlist_tracks.get_playlist_tracks(user_id,playlist_id, cid, secret)
 
 write_results.write_results(spotify_playlist_songs)
 
