229b1ba2fa452b3f093acf0474d9fc46e4b4e3b0
soonerfan237 <ou1/s00n>
2021-08-27 16:01:26 -0500

    Reading paginated results of playlist

--- spotipy_test.py
+++ spotipy_test.py
@@ -1,5 +1,5 @@
-import pandas as pd
 import spotipy
+
 sp = spotipy.Spotify()
 from spotipy.oauth2 import SpotifyClientCredentials
 cid = '5553faba0c3a447abc4eb54bda441322'
@@ -7,10 +7,8 @@ secret = 'e961dd2b417344ed8b51e4eccc4c025d'
 client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 sp.trace=False
-playlist = sp.user_playlist("soonerfan237", "48JH4qE9hjDR1kQ8kW8XAP")
-songs = playlist["tracks"]["items"]
-ids = []
-for i in range(len(songs)):
-    ids.append(songs[i]["track"]["id"])
-features = sp.audio_features(ids)
-df = pd.DataFrame(features)
+results = sp.user_playlist_tracks("soonerfan237","48JH4qE9hjDR1kQ8kW8XAP")
+tracks = results['items']
+while results['next']:
+    results = sp.next(results)
+    tracks.extend(results['items'])
